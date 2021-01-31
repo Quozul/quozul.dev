5671683c5af2852fb67fb8aa224c149915acf551
Erwan <erwan.legloannec@outlook.com>
2021-01-31 14:48:38 +0100

    Updated DHStats

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,10 @@
 <head>
     <title><?=$page_name?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/bootstrap/bootstrap.min.css">
-    <script src="/bootstrap/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/bootstrap/bootstrap-q.min.css">
+    <script src="/bootstrap/bootstrap-q.bundle.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/png" href="https://cdn.quozul.dev/assets/icon.png">
+    <link rel="icon" type="image/png" href="/assets/icon.png">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
     <script src="/scripts/utils.js"></script>
     <style>
