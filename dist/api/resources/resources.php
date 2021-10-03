d7f3485ef63dd42366872179a93376865dfb2840
Erwan <erwan.legloannec@outlook.com>
2021-10-03 17:07:48 +0200

    update: updated /api/resources endpoint

--- dist/api/resources/resources.php
+++ dist/api/resources/resources.php
@@ -1,91 +1,87 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] !== "GET") {
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     http_response_code(405);
     exit();
 }
 
-//ini_set("display_errors", true);
+//ini_set('display_errors', true);
 
-function recursiveDirScan(string $directory, ?string $user_id, ?string $view_mode = "default"): array
+function parseMetadata(string $path): array
+{
+    if (file_exists($path . '/.metadata.json')) {
+        return json_decode(file_get_contents($path . '/.metadata.json'), true);
+    }
+
+    return [];
+}
+
+function recursiveDirScan(string $directory, ?string $user_id, ?string $viewMode = 'default'): array
 {
     $files = [];
     $dirContent = scandir($directory);
 
     foreach ($dirContent as $file) {
-        if (str_starts_with($file, ".")) continue;
+        if (str_starts_with($file, '.')) continue;
 
         $path = $directory . '/' . $file;
 
+        $size = filesize($path);
+        $time = filemtime($path);
+
+        $data = [
+            'name' => $file,
+            'size' => $size,
+            'time' => $time,
+            'hasThumbnail' => false,
+        ];
+
         // TODO: Put metadata for folders in database
         if (is_dir($path)) {
-            $metadata = [];
-            if (file_exists($path . '/.metadata.json')) {
-                $metadata = json_decode(file_get_contents($path . '/.metadata.json'), true);
+            $metadata = parseMetadata($path);
 
-                if (
-                    isset($metadata["restricted"]) && $metadata["restricted"] && // If folder is restricted in access
-                    (is_null($user_id) || array_search($user_id, AUTHORIZED_IDS)) // If user is allowed to view this folder
-                ) continue;
-            }
-
-            $size = 0;
-            $content = recursiveDirScan($path, $user_id, $view_mode);
-            foreach ($content as $c) $size += $c["size"];
-
-            $data = [
-                "name" => $file,
-                "dir" => true,
-                "elements" => count($content),
-                "size" => $size,
-            ];
-
-            if (!empty($metadata)) $data["metadata"] = $metadata;
-
-            // Get episodes in seasons
-            switch ($view_mode) {
-                case "library": {
-                    $seasons = [];
-                    foreach ($content as $season) {
-                        if (isset($season["dir"])) $seasons[] = $season["elements"];
-                    }
-
-                    $data["seasons"] = $seasons;
-
-                    $thumbnail_path = $path . "/" . $content[0]["name"];
-                    if (file_exists($thumbnail_path . "/.thumbnail.jpg") || file_exists($thumbnail_path . "/.thumbnail.png")) {
-                        $data["has_thumbnail"] = true;
-                    }
-                    break;
-                }
-                default: {
-                    if (file_exists($path . "/.thumbnail.jpg") || file_exists($path . "/.thumbnail.png")) {
-                        $data["has_thumbnail"] = true;
-                    }
-                }
-            }
-
-            array_push($files, $data);
-        } else if ($file !== '.metadata.json') {
-            if ($view_mode !== "default" &&
-                (preg_match("/_dash(init)?/", $path) ||
-                preg_split("/\\//", mime_content_type($path))[0] !== "video")
+            if (
+                isset($metadata['restricted']) && $metadata['restricted'] && // If folder is restricted in access
+                (is_null($user_id) || array_search($user_id, AUTHORIZED_IDS)) // If user is allowed to view this folder
             ) continue;
 
-            $size = filesize($path);
-            $time = filemtime($path);
+            $data['metadata'] = $metadata;
 
-            $data = [
-                "name" => $file,
-                "size" => $size,
-                "time" => $time,
-            ];
+            $size = 0;
+            $content = recursiveDirScan($path, $user_id, $viewMode);
+            foreach ($content as $c) $size += $c['size'];
 
-            if (file_exists($path . ".png")) {
-                $data["has_thumbnail"] = true;
+            $data['dir'] = true;
+            $data['elements'] = count($content);
+            $data['size'] = $size;
+
+            // Get episodes in seasons
+            switch ($viewMode) {
+                case 'library': {
+                    $seasons = [];
+                    foreach ($content as $season) {
+                        if (isset($season['dir'])) {
+                            $seasons[] = $season['elements'];
+                        }
+                    }
+
+                    $data['seasons'] = $seasons;
+                    break;
+                }
             }
 
-            array_push($files, $data);
+            $thumbnailPath = $path . '/' . $content[0]['name'];
+            if (file_exists($path . '/.thumbnail.jpg') || file_exists($path . '/.thumbnail.png')) {
+                $data['hasThumbnail'] = true;
+            } else if (file_exists($thumbnailPath . '/.thumbnail.jpg') || file_exists($thumbnailPath . '/.thumbnail.png')) {
+                $data['hasThumbnail'] = true;
+            }
+
+        } else if ($file !== '.metadata.json') {
+            $mime = mime_content_type($path);
+            if ($viewMode !== 'default' && (!$mime || explode('/', $mime, 1)[0] !== 'video')) continue;
         }
+
+        array_push($files, $data);
     }
 
     return $files;
@@ -93,13 +89,13 @@ function recursiveDirScan(string $directory, ?string $user_id, ?string $view_mod
 
 header('Content-Type: application/json; charset=utf-8');
 
-$path = $_GET["path"] ?? "/";
-$re = "/(^|[\/\\\])(\.\.[\/\\\])+/";
-$real_path = preg_replace($re, "/", $path);
-$dir = getenv("PUBLIC_FOLDER") . $real_path;
+$path = $_GET['path'] ?? '/';
+$re = '/(^|[\/\\\])(\.\.[\/\\\])+/';
+$real_path = preg_replace($re, '/', $path);
+$dir = getenv('PUBLIC_FOLDER') . $real_path;
 
 // Get user's JWT
-require_once __DIR__ . "/../utils.php";
+require_once __DIR__ . '/../utils.php';
 $id = getUserId();
 
 if (!file_exists($dir)) {
@@ -118,24 +114,24 @@ if (!verifyParentFolders($path, $id)) {
 }
 
 $metadata = null;
-$view_mode = "default";
-$data = [ "path" => $real_path ];
+$viewMode = 'default';
+$data = [ 'path' => $real_path ];
 
 if (file_exists($dir . '/.metadata.json')) {
     $metadata = json_decode(file_get_contents($dir . '/.metadata.json'), true);
 
     if (!empty($metadata)) {
-        $data["metadata"] = $metadata;
-        if (isset($metadata["view_mode"])) {
-            $view_mode = $metadata["view_mode"];
+        $data['metadata'] = $metadata;
+        if (isset($metadata['viewMode'])) {
+            $viewMode = $metadata['viewMode'];
         }
     }
 }
 
-$files = recursiveDirScan($dir, $id, $view_mode);
+$files = recursiveDirScan($dir, $id, $viewMode);
 usort($files, function ($a, $b) {
-    return isset($b["dir"]) && !isset($a["dir"]);
+    return isset($b['dir']) && !isset($a['dir']);
 });
-$data["content"] = $files;
+$data['content'] = $files;
 
 echo json_encode($data);
